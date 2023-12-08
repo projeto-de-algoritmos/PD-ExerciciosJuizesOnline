d0fa46ca7e7b8c7b08cd426f7f8da5d2dacf99f6
fellipepcs <fellipy.02@hotmail.com>
2023-12-07 21:40:48 -0300

    feat: adicionado coin_change com erro

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 O repositório contém soluções para algumas questões de juíz online. Segue a lista dos exercicios escolhidos:
 
-- Exercicio 1 - (https://exercicio1.com)
+- Exercicio 1 - (https://leetcode.com/problems/coin-change/description/)
 - Exercicio 2 - (https://leetcode.com/problems/cherry-pickup-ii/description/)
 - Exercicio 3 - (https://exercicio1.com)
 - Exercicio 4 - (https://exercicio1.com)
---
+++ exercicio1/coin_change.py
@@ -0,0 +1,35 @@
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        # Dicionário para o armazenamento dos valores já analisados
+        solucoes = {}
+
+        def rec(amount):
+            # Se o valor a ser alcançado já foi analisado, a 
+            # resposta já está contida no dicionário de soluções
+            if amount in solucoes:
+                return solucoes[amount]
+            
+            # Se o valor a ser alcançado é 0, a resposta é 0 pois
+            # serão utilizadas 0 moedas
+            if amount == 0:
+                return 0
+
+            moedasUtilizadas = []
+            # Analisa cada uma das possiveis moedas
+          
+            # Adiciona-se 1 ao menor número encontrado para cobrir o valor,
+            # correspondendo a moeda que foi utilizada para "zerar" o montante 
+            menorQuantidade = min(moedasUtilizadas) + 1
+            # Adiciona ao dicionário de soluções o número de moedas necessárias 
+            # para o valor atual
+            solucoes[amount] = menorQuantidade
+            # Retorna o menor número de moedas para suprir o valor atual
+            return menorQuantidade
+
+        resultado = rec(amount)
+
+        # Se o resultado não for infinito, foi encontrada uma solução    
+        if resultado != float('inf'):
+            return resultado
+        # Se não foi encontrada uma solução, retorna -1
+        return -1
