7ea72e769a2b6d4036b1f42940a5ca0de7d02e7f
LamequeFernandes <lamequesao@gmail.com>
2023-12-11 21:18:33 -0300

    melhoria em exercicio 3

--- exercicio3/maximum_profit.py
+++ exercicio3/maximum_profit.py
@@ -1,11 +1,17 @@
 class Solution(object):
     def jobScheduling(self, startTime, endTime, profit):
+        # Lista de tupla com os jobs por ordem de fim
         jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
-        n = len(jobs)
-        dp = [0] * (n + 1)
-        p = [0] * (n + 1)
+        qtd_jobs = len(jobs)
 
-        for i in range(n - 1, -1, -1):
+        # armazena as soluçÕes parciais do problema
+        dp = [0] * (qtd_jobs + 1)
+
+        # armazena o índice do último trabalho compatível com o trabalho i, ou 0 se nenhum trabalho anterior for compatível.
+        p = [0] * (qtd_jobs + 1)
+
+        # tenta encontrar o trabalho mais recente que é compatível com o trabalho atual
+        for i in range(qtd_jobs - 1, -1, -1):
             j = i - 1
             while j >= 0 or p[i] != 0:
                 if jobs[j][1] <= jobs[i][0]:
@@ -13,8 +19,24 @@ class Solution(object):
                     break
                 j -= 1
 
+        # Define o caso base, indicando que o máximo lucro obtido até o momento 0 (nenhum trabalho) é 0.
         dp[0] = 0
-        for i in range(1, n + 1):
+
+        for i in range(1, qtd_jobs + 1):
             dp[i] = max(jobs[i - 1][2] + dp[p[i]], dp[i - 1])
 
         return max(dp)
+
+
+if __name__ == '__main__':
+    solver = Solution()
+
+    startTime = input()
+    endTime = input()
+    profit = input()
+
+    print(solver.jobScheduling(
+        list(map(int, startTime.split(','))), 
+        list(map(int, endTime.split(','))) , 
+        list(map(int, profit.split(',')))
+    ))
